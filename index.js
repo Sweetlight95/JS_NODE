6ce7f8d0fabb98d11e3d3f82e343ff71c60157b3
Sweetlight95 <princessadesuwa93@gmail.com>
2022-01-14 00:19:15 +0100

    new

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ const url = require('url');
 // console.log(req.url);
 ////////////////////////////////////////////////////
 //SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -40,8 +43,16 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT')
-    } else{
-        res.end('Page not found')
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(data)
+    }
+    else {
+        // res.writeHead(404, { 
+        //     'Content-type': 'text/html',
+        //     'my-own-header': 'hello-world'
+        // })
+        res.end('<h1>Page not found!</h1>')
     }
 });
 
